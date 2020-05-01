a7a985c78fff7ea603c1808136ed4f46f9531287
Dhiraj Garg <dhirajgarg@Dhirajs-MacBook-Pro.local>
2020-05-01 15:55:58 -0700

    done

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import app
 from db import db
+
+
+db.init_app(app)
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-db.init_app(app)
 
-app.run(port = 5000,debug= True)
